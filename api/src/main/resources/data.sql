f23e7ae08067c799216d59447fd2700657b5d8b4
dialsy <149670407+dialsy@users.noreply.github.com>
2023-11-10 17:20:45 +0100

    Update data.sql

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -12,3 +12,26 @@ INSERT INTO employees (first_name, last_name, mail, password) VALUES
   ('Laurent', 'GINA', 'laurentgina@mail.com', 'laurent'),
   ('Sophie', 'FONCEK', 'sophiefoncek@mail.com', 'sophie'),
   ('Agathe', 'FEELING', 'agathefeeling@mail.com', 'agathe');
+
+CREATE TABLE freelancers (
+  id VARCHAR(50) PRIMARY KEY,
+  name VARCHAR(250) NOT NULL,
+  job VARCHAR(250) NOT NULL,
+  picture VARCHAR(500) NOT NULL,
+  location VARCHAR(250) NOT NULL,
+  available BOOLEAN NOT NULL,
+  tjm INT NOT NULL
+);
+
+INSERT INTO freelancers (id, name, job, picture, location, available, tjm) VALUES
+  ('1', 'Julien Brun', 'Développeur mobile', 'http://localhost:8000/images/4.jpeg', 'Lyon', true, 500),
+  ('2', 'Arielle Gautier', 'Développeuse fullstack', 'http://localhost:8000/images/1.jpeg', 'Paris', false, 620),
+  ('3', 'Marine Carpentier', 'Développeuse frontend', 'http://localhost:8000/images/2.jpeg', 'Bordeaux', true, 520),
+  ('4', 'Lucille Barre', 'Product Designer', 'http://localhost:8000/images/3.jpeg', 'Lille', false, 650),
+  ('5', 'Clément Rolland', 'Développeur mobile', 'http://localhost:8000/images/5.jpeg', 'Lyon', false, 450),
+  ('6', 'Grégoire Chevalier', 'Développeur backend', 'http://localhost:8000/images/6.jpeg', 'Paris', true, 510),
+  ('7', 'Raphaël Rodriguez', 'Designer', 'http://localhost:8000/images/7.jpeg', 'Paris', true, 480),
+  ('8', 'Hugo Vysa', 'Développeur frontend', 'http://localhost:8000/images/8.jpeg', 'Toulouse', false, 560),
+  ('9', 'Mina Toman', 'Développeuse Mobile', 'http://localhost:8000/images/11.jpeg', 'Bayonne', true, 630),
+  ('10', 'Amélie Leroy', 'Développeuse backend', 'http://localhost:8000/images/12.jpeg', 'Paris', false, 400),
+  ('11', 'Maxime Lebrun', 'Intégrateur SEO', 'http://localhost:8000/images/9.jpeg', 'Rennes', false, 600);
